ee620218ece4ad3d68e00bfe1188d7a7cf1729a4
Linxhs77 <tutarerufagamerbr@gmail.com>
2025-03-10 09:07:05 -0300

    feat: classes Escola, Professor e aluno

---
+++ ApiSegundoDS/Models/Escola.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiSegundoDS.Models
+{
+    public class Escola
+    {
+
+        public int id
+        { get; set: }
+        public string? Nome { get; set; }
+        public Professor Professor { get; set; }
+
+
+    }
+}
---
+++ ApiSegundoDS/Models/Professor.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiSegundoDS.Models
+{
+    public class Professor
+    {
+
+        public int id
+        { get; set: }
+        public string? Nome { get; set; }
+        public string? RMA { get; set; }
+        public string? Mae { get; set; }
+        public string? Pai { get; set; }
+        public Escola Escola { get; set; }
+        public Aluno Aluno { get; set; }
+
+    }
+}
